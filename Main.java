e04e19ca1d2126b8c181018d2924a3b1d231804d
Laude44 <halidahmet44@gmail.com>
2025-12-12 16:58:43 +0300

    Nearly compleated the project. 1 method and loading method remain

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
     public static String mostProfitableCommodityInMonth(int month) {
-        if(month<0 || month > 12) return "INVALID_MONTH";
+        if(month<0 || month > 11) return "INVALID_MONTH";
         int mostProfComIndex=0;
         int mostProfComProfit=0;
 
@@ -36,7 +36,7 @@ public class Main {
     }
 
     public static int totalProfitOnDay(int month, int day) {
-        if(month<0 || month > 12 || day < 0 || day >28){return ERROR;}
+        if(month<0 || month > 11 || day < 0 || day >28){return ERROR;}
         int total=0;
         for(int i=0;i<COMMS;i++){
             total += dataMemory[month][day][i];
@@ -61,7 +61,7 @@ public class Main {
     }
 
     public static int bestDayOfMonth(int month) { 
-        if(month > 12 || month < 0) {return -1;}
+        if(month > 11 || month < 0) {return -1;}
         int bestDayIndex=-1;
         int bestDayTotal=0;
 
@@ -88,37 +88,108 @@ public class Main {
         for(int i=0;i<12;i++){
             int monthTotal=0;
             for(int j=0;j<28;j++){
-                monthTotal=dataMemory[i][j][commodityIndex];
+                monthTotal += dataMemory[i][j][commodityIndex];
             }
             if(monthTotal>bestMonthTotal) {
                 bestMonthIndex=i;
                 bestMonthTotal=monthTotal;
             }
         }
-
-
-
-
         return commodities[bestMonthIndex];
     }
 
-    public static int consecutiveLossDays(String comm) { 
-        return 1234; 
+    public static int consecutiveLossDays(String comm) {
+        int commodityIndex =-1;
+        for(int i=0;i<commodities.length;i++){
+            if(comm.equals(commodities[i])){commodityIndex=i;}
+        }
+        if(commodityIndex==-1){return -1;}
+        int loseStreak =0;
+        for(int i=0;i<12;i++){
+            for(int j=0;j<28;j++){
+                if(dataMemory[i][j][commodityIndex]<0){
+                    loseStreak++;
+                }
+                else{loseStreak=0;
+                }
+            }
+        }
+        return loseStreak;
     }
     
-    public static int daysAboveThreshold(String comm, int threshold) { 
-        return 1234; 
+    public static int daysAboveThreshold(String comm, int threshold) {
+        int commodityIndex =-1;
+        for(int i=0;i<commodities.length;i++){
+            if(comm.equals(commodities[i])){commodityIndex=i;}
+        }
+        if(commodityIndex==-1){return -1;}
+        int thresholdCounter=0;
+        for(int i=0;i<12;i++){
+            for(int j=0;j<28;j++){
+                if(dataMemory[i][j][commodityIndex]>threshold){
+                    thresholdCounter++;
+                }
+            }
+        }
+        return thresholdCounter;
     }
 
-    public static int biggestDailySwing(int month) { 
-        return 1234; 
+    public static int biggestDailySwing(int month) {
+        if (month < 0 || month > 11) return ERROR;
+
+        int previousDayTotal = 0;
+        int biggestSwing = 0;
+
+        for (int j=0;j<5;j++) {
+            previousDayTotal += dataMemory[month][0][j];
+        }
+
+        for (int i=1;i<28;i++) {
+            int daysTotal = 0;
+            for (int j=0;j<5;j++) {
+                daysTotal += dataMemory[month][i][j];
+            }
+            int swing = Math.abs(daysTotal - previousDayTotal);
+            if (swing > biggestSwing) biggestSwing = swing;
+            previousDayTotal = daysTotal;
+        }
+
+        return biggestSwing;
+    }
+
+    public static String compareTwoCommodities(String c1, String c2) {
+        int commodityIndexc1 =-1;
+        int commodityIndexc2 =-1;
+        for(int i=0;i<commodities.length;i++){
+            if(c1.equals(commodities[i])){commodityIndexc1=i;}
+        }
+        for(int i=0;i<commodities.length;i++){
+            if(c2.equals(commodities[i])){commodityIndexc2=i;}
+        }
+        if(commodityIndexc1==-1 || commodityIndexc2==-1){return "INVALID_COMMODITY";}
+        int c1Total =0;
+        int c2Total =0;
+        for(int i=0;i<12;i++){
+            for(int j=0;j<28;j++){
+                c1Total += dataMemory[i][j][commodityIndexc1];
+                c2Total += dataMemory[i][j][commodityIndexc2];
+            }
+        }
+        if(c1Total>c2Total){return c1+" is better than "+c2;}
+        else if(c1Total<c2Total){return c2+" is better than "+c1;}
+        else {return c1+" is equal to "+c2;}
     }
     
-    public static String compareTwoCommodities(String c1, String c2) { 
-        return "DUMMY is better by 1234"; 
-    }
-    
-    public static String bestWeekOfMonth(int month) { 
+    public static String bestWeekOfMonth(int month) {
+        if(month > 11 || month < 0) {return "INVALID_MONTH";}
+        for(int i =0;i<4;i++){
+            int bestWeek=0;
+            for(int j=0;j<7;j++){
+
+            }
+        }
+
+
         return "DUMMY"; 
     }
 
